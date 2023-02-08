b340100abda64a4f02951534a22fa6725274073f
Scott Jasperse <scott.jasperse@gmail.com>
2023-02-07 21:09:04 -0500

    Adding  path planner paths and auton chooser

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private CommandBase m_autonomousCommand;
+  private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
   /**
@@ -27,7 +27,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
   }
 
   /**
@@ -57,6 +56,8 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,30 +5,34 @@
 package frc.robot;
 
 import java.util.Arrays;
-
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.FollowTrajectoryCommand;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class RobotContainer {
   private final Drivetrain m_drivetrain = new Drivetrain();
   private final XboxController m_controller = new XboxController(0);
   private final Field2d m_field = new Field2d();
-  
+  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
+
   public RobotContainer() {
     SmartDashboard.putData("Field", m_field);
-    
+
+    loadTrajectories(m_chooser);
+    SmartDashboard.putData("Auton", m_chooser);
+
     configureButtonBindings();
   }
 
@@ -39,8 +43,14 @@ public class RobotContainer {
   private void configureButtonBindings() {
   }
 
-  public CommandBase getAutonomousCommand() {
-    m_drivetrain.reset();
+  private void loadTrajectories(SendableChooser<Command> chooser) {
+    var path1Trajectory = PathPlanner.loadPath("Path1", new PathConstraints(4, 3));
+    var trajectory1Command = new FollowTrajectoryCommand(m_drivetrain, path1Trajectory);
+    chooser.addOption("Path 1", trajectory1Command);
+
+    var path2Trajectory = PathPlanner.loadPath("Path2", new PathConstraints(4, 3));
+    var trajectory2Command = new FollowTrajectoryCommand(m_drivetrain, path2Trajectory);
+    chooser.addOption("Path 2", trajectory2Command);
 
     TrajectoryConfig config = new TrajectoryConfig(
         Units.feetToMeters(2.0), Units.feetToMeters(2.0));
@@ -48,19 +58,25 @@ public class RobotContainer {
 
     var hundredInches = Units.inchesToMeters(100);
 
-    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+    var simpleTrajectory = TrajectoryGenerator.generateTrajectory(
         Arrays.asList(
-          new Pose2d()
-          , new Pose2d(hundredInches, 0, new Rotation2d())
-          // , new Pose2d(hundredInches, hundredInches, new Rotation2d(180))
-          // , new Pose2d(hundredInches, -hundredInches, new Rotation2d())
-          // , new Pose2d(hundredInches, 0, new Rotation2d())
-          // , new Pose2d(0,0, new Rotation2d())
+            new Pose2d(), new Pose2d(hundredInches, 0, new Rotation2d())
+        // , new Pose2d(hundredInches, hundredInches, new Rotation2d(180))
+        // , new Pose2d(hundredInches, -hundredInches, new Rotation2d())
+        // , new Pose2d(hundredInches, 0, new Rotation2d())
+        // , new Pose2d(0,0, new Rotation2d())
         ),
-        config
-    );
+        config);
+    var simpleTrajectoryCommand = new FollowTrajectoryCommand(m_drivetrain, simpleTrajectory);
+    chooser.addOption("Path 3", simpleTrajectoryCommand);
+    
+    m_chooser.setDefaultOption("Path 1", trajectory1Command);
+  }
 
-    return new FollowTrajectoryCommand(m_drivetrain, trajectory);
+  public Command getAutonomousCommand() {
+    m_drivetrain.reset();
+
+    return m_chooser.getSelected();
   }
 
   public Command getArcadeDriveCommand() {
